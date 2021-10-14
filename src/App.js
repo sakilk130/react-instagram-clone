3c02eb7c8a1a3bef679e91e697f9cc271dc32120
Sakil Khan <sakilk130@gmail.com>
2021-10-14 12:37:11 +0600

    create post component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
 
+import Post from './components/Post';
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
       <h1>Hello from react</h1>
+
+      <Post
+        username="sakilkhan"
+        caption="Instagram"
+        imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+      />
+
+      <Post
+        username="sakil"
+        caption="clone Instagram"
+        imageUrl="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
+      />
+      <Post
+        username="sony"
+        caption="Post from sony"
+        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpm0AXv9uGZD7IQKGrI10kmGPXytkQxo_t_gWCJvbt6QFL9VcWHCBnLT3sF2OXrv6xme4&usqp=CAU"
+      />
     </div>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+
+import '../Post.css';
+
+function Post({ username, caption, imageUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar className="post__avatar" alt={username} src="" />
+        <h3>{username}</h3>
+      </div>
+      <img className="post__image" src={imageUrl} alt="" />
+      <h4 className="post__text">
+        <strong>{username}</strong> {caption}
+      </h4>
+    </div>
+  );
+}
+
+export default Post;

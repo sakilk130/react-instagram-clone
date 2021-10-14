ab937d4f1ae7d4bec3f3200a5dc50f3e6742c244
Sakil Khan <sakilk130@gmail.com>
2021-10-14 15:57:54 +0600

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
+import './App.css';
+import { db } from './firebase/config.js';
 import Post from './components/Post';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  });
+
   return (
     <div className="app">
       <div className="app__header">
@@ -13,23 +28,14 @@ function App() {
         />
       </div>
       <h1>Hello from react</h1>
-
-      <Post
-        username="sakilkhan"
-        caption="Instagram"
-        imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-      />
-
-      <Post
-        username="sakil"
-        caption="clone Instagram"
-        imageUrl="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
-      />
-      <Post
-        username="sony"
-        caption="Post from sony"
-        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpm0AXv9uGZD7IQKGrI10kmGPXytkQxo_t_gWCJvbt6QFL9VcWHCBnLT3sF2OXrv6xme4&usqp=CAU"
-      />
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
